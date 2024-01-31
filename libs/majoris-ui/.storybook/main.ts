d02f424c422ce7c5aa604b3db2622faa2f58ce84
psbrunosouza <psbrunosouza@gmail.com>
2024-01-31 20:16:22 -0300

    feat: mjs-field

--- libs/majoris-ui/.storybook/main.ts
+++ libs/majoris-ui/.storybook/main.ts
@@ -4,6 +4,7 @@ import path from 'path';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
+    '@storybook/addon-interactions',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-docs',
---
+++ libs/majoris-ui/src/lib/components/field/field.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mjs-field',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './field.component.html',
+  styleUrl: './field.component.scss',
+})
+export class FieldComponent {}
