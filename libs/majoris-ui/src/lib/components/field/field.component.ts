2d814ed8925d4d76154d9dc7e08d3bd59cbd4d12
psbrunosouza <psbrunosouza@gmail.com>
2024-02-01 09:39:03 -0300

    feat: add mjs input field

--- libs/majoris-ui/src/lib/components/field/field.component.ts
+++ libs/majoris-ui/src/lib/components/field/field.component.ts
@@ -1,11 +1,67 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Optional,
+  Self,
+  booleanAttribute,
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NgControl,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'mjs-field',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './field.component.html',
   styleUrl: './field.component.scss',
 })
-export class FieldComponent {}
+export class FieldComponent implements ControlValueAccessor {
+  @Input({ transform: booleanAttribute }) required = false;
+
+  @Input({ transform: booleanAttribute }) readonly = false;
+
+  @Input({ transform: booleanAttribute }) disabled = false;
+
+  @Input() blur: EventEmitter<boolean> = new EventEmitter();
+
+  text: string = '';
+
+  constructor(@Optional() @Self() public ngControl: NgControl) {
+    this.ngControl && (this.ngControl.valueAccessor = this);
+  }
+
+  public onChangeFn = (_: any) => {};
+
+  public onTouchedFn = () => {};
+
+  writeValue(text: string): void {
+    this.text = text;
+  }
+
+  public registerOnChange(fn: any): void {
+    this.onChangeFn = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouchedFn = fn;
+  }
+
+  public onChange() {
+    this.onChangeFn(this.text);
+  }
+
+  public onBlur() {
+    this.onTouchedFn();
+    this.blur.emit();
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+}
